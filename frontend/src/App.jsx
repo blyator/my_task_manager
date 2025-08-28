206e5b362a796112ac632db3e08f0c9f60cb004a
Billy Yator <dmnbilly@gmail.com>
2025-08-28 13:48:50 +0300

    fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const BASE_URL = "http://127.0.0.1:8000/api";
+
+const App = () => {
+  const [taskData, setTaskData] = useState({
+    count: 0,
+    next: null,
+    previous: null,
+    results: [],
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${BASE_URL}/tasks/`)
+      .then((response) => {
+        setTaskData(response.data);
+      })
+      .catch((error) => console.error("Error fetching tasks:", error))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-50">
+      <div className="p-4 sm:p-6 max-w-4xl mx-auto">
+        <TaskForm />
+        <TaskList data={taskData} loading={loading} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
